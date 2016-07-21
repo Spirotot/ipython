c91545bd1eaa9bb897d83748d749c60ab6e8e232
Jason Paryani <github@jparyani.com>
2016-07-21 15:00:10 -0700

    Hide new terminal button since it doesn't work

--- .sandstorm/custom.js
+++ .sandstorm/custom.js
@@ -1,4 +1,9 @@
 define(['base/js/namespace'], function(Jupyter){
   // Prevent notebooks from opening in a new window.
   Jupyter._target = '_self';
+
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = "#new-terminal { display: none; }";
+  document.head.appendChild(css);
 });
